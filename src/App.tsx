65a78d9aa04d58e76988693f1327ec5bfee85064
s.takahashi <shuigaoqiao9@gmail.com>
2022-11-03 11:05:18 +0900

    カスタムフックについて

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { UserCard } from './components/UserCard';
+import { useAllUsers } from './hooks/useAllUsers';
+/**
+ * カスタムフック
+ * 関数
+ * hooksの各機能を使用
+ * コンポーネントからロジックを分離
+ * 使い回し、テスト容易性、見通しが良くなる
+ * 自由に作れる use~という名前にする
+ */
 function App() {
+  const { getUsers, userProfiles, loading, error } = useAllUsers()
+
+  const onClickFetchUser = () => {
+    getUsers()
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="">
+      <button onClick={onClickFetchUser}>ユーザー情報取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>エラーあり</p>
+      ) : loading ? (
+        <p>ローディング</p>
+      ) : (
+        <>
+          {
+            userProfiles.map((user) => (
+              <UserCard key={user.id} user={user}></UserCard>
+            ))
+          }
+        </>
+      )
+      }
+
+    </div >
   );
 }
 
